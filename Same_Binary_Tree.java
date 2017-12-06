43fbe75678e18eb8a62117ccf57dbb2c5738a6e3
vipul511 <valiavipul51@gmail.com>
2017-12-05 20:33:47 -0500

    Add files via upload

---
+++ Same_Binary_Tree.java
@@ -0,0 +1,44 @@
+
+public class Same_Binary_Tree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Node a1 = new Node(10);
+		Node b1 = new Node(-5);
+		Node c1 = new Node(-10);
+		Node d1 = new Node(5);
+		Node e1 = new Node(25);
+		Node f1 = new Node(36);
+		a1.left = b1;
+		b1.left = c1;
+		b1.right = d1;
+		a1.right = e1;
+		e1.right = f1;
+		Node a2 = new Node(10);
+		Node b2 = new Node(-5);
+		Node c2 = new Node(-10);
+		Node d2 = new Node(5);
+		Node e2 = new Node(25);
+		Node f2= new Node(36);
+		a2.left = b2;
+		b2.left = c2;
+		b2.right = d2;
+		a2.right = e2;
+		e2.right = f2;
+		boolean found1 = same(a1,a2);
+		System.out.println(found1);
+
+	}
+
+	private static boolean same(Node root1, Node root2) {
+		// TODO Auto-generated method stub
+		if(root1== null && root2== null) {
+			return true;
+		}
+		if(root1 == null || root2 == null) {
+			return false;
+		}
+		return root1.data == root2.data && same(root1.left,root2.left) && same(root1.right, root2.right);
+	}
+
+}
